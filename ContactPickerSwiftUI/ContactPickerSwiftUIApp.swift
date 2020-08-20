95dc57f58469fa918abb89bd8f665e16bb1823f7
vollkorntomate <thomas.voelkl@outlook.de>
2020-08-20 21:19:11 +0200

    Initial Commit

---
+++ ContactPickerSwiftUI/ContactPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactPickerSwiftUIApp.swift
+//  ContactPickerSwiftUI
+//
+//  Created by Thomas Völkl on 20.08.20.
+//
+
+import SwiftUI
+
+@main
+struct ContactPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactPickerSwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ContactPickerSwiftUI
+//
+//  Created by Thomas Völkl on 20.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
